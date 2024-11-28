19eb61ba6e2bd25c08925b8b4692e05fbc0a9f67
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-11-28 15:41:03 -0600

    Update about.js

--- about.js
+++ about.js
@@ -1,5 +1,3 @@
-// Currently no dynamic functionality needed for the About page.
-// You can add interactivity here if required in the future.
 document.addEventListener('DOMContentLoaded', function() {
     console.log('About page loaded');
 });
